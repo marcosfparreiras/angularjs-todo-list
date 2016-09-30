eca15fc1ea420e73384b532414b2e4601a5eb4f9
marcosfparreiras <marcosfparreiras@gmail.com>
2016-09-29 22:57:25 -0300

    Mark done tasks

--- todo.js
+++ todo.js
@@ -1,10 +1,9 @@
 var app = angular.module('app', []);
 app.controller('Todo', function($scope) {
     $scope.test = 3;
-    // $scope.lastName = "Doe";
+
+    $scope.todos = [
+      {text:'First task, done:false'},
+      {text:'Second task, done:false'}
+    ];
 });
-
-// function test($scope) {
-//   $scope.t = 4;
-
-// }
