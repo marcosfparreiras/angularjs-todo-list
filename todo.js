b3f5d72ba0cdfdd3e2cbe96e2c981f725db59d85
marcosfparreiras <marcosfparreiras@gmail.com>
2016-09-29 23:11:50 -0300

    Implement adding new todo

--- todo.js
+++ todo.js
@@ -2,8 +2,21 @@ var app = angular.module('app', []);
 app.controller('Todo', function($scope) {
     $scope.test = 3;
 
+    $scope.getTotalTodos = function() {
+      return $scope.todos.length;
+    }
+
     $scope.todos = [
       {text:'First task, done:false'},
       {text:'Second task, done:false'}
     ];
+
+    $scope.addTodo = function() {
+      var newTodo = {
+        text: $scope.formTodoText,
+        done: false
+      }
+      $scope.todos.push(newTodo);
+      $scope.formTodoText = "";
+    }
 });
