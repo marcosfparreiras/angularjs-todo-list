699af2e679ed49ed718ce83c7b0f7e36cb8eaaf6
marcosfparreiras <marcosfparreiras@gmail.com>
2016-09-29 22:43:48 -0300

    Create structure with controller

---
+++ todo.js
@@ -0,0 +1,10 @@
+var app = angular.module('app', []);
+app.controller('Todo', function($scope) {
+    $scope.test = 3;
+    // $scope.lastName = "Doe";
+});
+
+// function test($scope) {
+//   $scope.t = 4;
+
+// }
